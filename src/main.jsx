b6a8571a3aa4760de004eb197b750a59a930e3bf
shadab <gabuyama68@gmail.com>
2023-10-02 19:59:13 +0530

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
-import App from "./App";
 
+//this is main wayyeasy-web
+import App from "./App";
 import store from "./redux/store";
 
 ReactDOM.render(
